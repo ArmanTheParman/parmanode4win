6faa5000b8e76092f0840f46d0f63e8590e9fee9
Parman <armantheparman@protonmail.com>
2024-07-25 15:00:29 +0100

    a

--- src/menus/menu_use_f.py
+++ src/menus/menu_use_f.py
@@ -1,2 +1,40 @@
+from config.variables_f import *
+from tools.screen_f import *
+
 def menu_use():
-    pass
+
+    while True:
+
+        if ico.grep("bitcoin-end"): 
+            use_bitcoin = f"#                  {green} (b){orange}            Bitcoin Core                                        #"
+            bitcoinmenu = True
+        else: 
+            use_bitcoin ="#                                                                                      #"
+            bitcoinmenu = False
+
+        print(f"""
+########################################################################################
+#                                                                                      #
+#    P A R M A N O D E --> Main Menu -->{cyan} Use Programs Menu {orange}                            #
+#                                                                                      #
+########################################################################################
+#                                                                                      #
+#                                                                                      #
+{use_bitcoin}
+#                                                                                      #
+#                                                                                      #
+########################################################################################
+""")
+        choice = choose("xpmq")
+        if choice in {"q", "Q", "Quit", "exit", "EXIT"}: 
+            quit()
+        elif choice in {"p", "P"}:
+            return True
+        elif choice in {"m", "M"}:
+            return True
+        elif choice.lower() in {"b", "bitcoin"}:
+            if bitcoinmenu == False: continue
+            if not use_bitcoin(): return False
+            return True
+        else:
+            invalid()
