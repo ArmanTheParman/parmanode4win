61cf3d994d1c9bb11558bd58954c5d3d07473d08
Parman <armantheparman@protonmail.com>
2024-07-29 03:28:13 +1000

    ico

---
+++ src/sparrow/__init__.py
@@ -0,0 +1,1 @@
+from .install_sparrow_f import *
--- src/sparrow/install_sparrow_f.py
+++ src/sparrow/install_sparrow_f.py
@@ -1,5 +1,4 @@
 from pmodules import *
-from sparrow.config import *
 
 def install_sparrow():
 
@@ -21,14 +20,16 @@ def install_sparrow():
 
     if not sparrowpath.exists():
         sparrowpath.mkdir()
-    
+    ico.add("sparrow-start") 
     if not download_sparrow(): return False #also extracts and moves, zip left, unzipped dir deleted.
 
     if not verify_sparrow(): return False
 
     if not make_sparrow_config(): return False
-    # does .sparrow wallet dir exist?
 
+    ico.add("sparrow-end")
+    success("Sparrow has been installed")
+    return True
 
 ########################################################################################
 ########################################################################################
@@ -152,3 +153,66 @@ both sha256 and gpg.{orange}
     else:
         announce(f"There was a problem verifying the SHA256SUMS file with Craig Raw's signature. Aborting.")
         return False
+
+def make_sparrow_config():
+
+    global sparrow_config1, sparrow_config2, sparrow_config3
+
+    sparrow_config_dir = HOME / "Appdata" / "Roaming" / "Sparrow"
+    sparrow_config_path = sparrow_config_dir / "config"
+    if not sparrow_config_dir.exists():
+        sparrow_config_dir.mkdir()
+
+    bitcoin_dir = pco.grep("bitcoin_dir=", returnline=True).strip().split('=')[1]
+    coreDD = ""
+    for i in bitcoin_dir:
+       if i == "\\":
+         i = '/'
+       coreDD = coreDD + i
+
+    sparrow_config = """{
+    "mode": "ONLINE",
+    "bitcoinUnit": "BTC",
+    "unitFormat": "DOT",
+    "blockExplorer": "https://mempool.space",
+    "feeRatesSource": "MEMPOOL_SPACE",
+    "fiatCurrency": "USD",
+    "exchangeSource": "COINGECKO",
+    "loadRecentWallets": true,
+    "validateDerivationPaths": true,
+    "groupByAddress": true,
+    "includeMempoolOutputs": true,
+    "notifyNewTransactions": true,
+    "checkNewVersions": false,
+    "theme": "LIGHT",
+    "openWalletsInNewWindows": false,
+    "hideEmptyUsedAddresses": false,
+    "showTransactionHex": true,
+    "showLoadingLog": true,
+    "showAddressTransactionCount": false,
+    "showDeprecatedImportExport": false,
+    "signBsmsExports": false,
+    "preventSleep": false,
+    "dustAttackThreshold": 1000,
+    "enumerateHwPeriod": 30,
+    "useZbar": true,
+    "serverType": "BITCOIN_CORE",
+    "publicElectrumServer": "ssl://bitcoin.lu.ke:50002|bitcoin.lu.ke",
+    "coreServer": "http://127.0.0.1:8332",
+    "coreAuthType": "USERPASS",
+    "coreDataDir": "{coreDD}",' 
+    "coreAuth": "parman:parman",
+    "useLegacyCoreWallet": false,
+    "useProxy": false,
+    "autoSwitchProxy": true,
+    "maxServerTimeout": 34,
+    "maxPageSize": 100,
+    "usePayNym": false,
+    "mempoolFullRbf": false,
+    "appWidth": 1083.0,
+    "appHeight": 805.5
+  }"""
+
+      
+    with sparrow_config_path.open('w') as f:
+        f.write(sparrow_config + '\n')
