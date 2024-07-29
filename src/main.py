10a06804da41c04063927441892dd7358b3b58b6
Parman <armantheparman@protonmail.com>
2024-07-30 04:10:36 +1000

    a

--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ from module_f import *
 
 def whatsmyip():
     USER = os.getenv('USERNAME')
+    print("Calling get_IP_variables()")
     get_IP_variables()  # Call the function to set global variables
     print(f"Debug in main: IP = {IP}")
 
