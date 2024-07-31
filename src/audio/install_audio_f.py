44ab6816f6a5e9da4ae7523693b2aa607b680136
Parman <armantheparman@protonmail.com>
2024-08-01 00:21:02 +1000

    a

--- src/audio/install_audio_f.py
+++ src/audio/install_audio_f.py
@@ -5,16 +5,23 @@ import subprocess
 
 def install_audio():
 
-    try: subprocess.run(["pip", "install", "pygame"], check=True)
-    try: subprocess.run(["pip", "install", "ffmpg"], check=True)
-    try: subprocess.run(["pip", "install", "moviepy"], check=True)
+    try: 
+        subprocess.run(["pip", "install", "pygame"], check=True)
+        subprocess.run(["pip", "install", "ffmpg"], check=True)
+        subprocess.run(["pip", "install", "moviepy"], check=True)
+
     except Exception as e: input(e)
+
     ico.add("audio-end")
     success("Audio for Parmanode has been installed")
 
 def uninstall_audio():
 
-    try: subprocess.run(["pip", "uninstall", "pgame"], check=True)
+    try: 
+        subprocess.run(["pip", "uninstall", "pgame"], check=True)
+        subprocess.run(["pip", "uninstall", "ffmpg"], check=True)
+        subprocess.run(["pip", "uninstall", "moviepy"], check=True)
+        
     except Exception as e: input(e)
     ico.remove("audio-")
     success("Audio for Parmanode has been uninstalled")
