59f6b2167396bcc5865c443a7801c1e8ffb536a4
Parman <armantheparman@protonmail.com>
2024-07-31 23:49:15 +1000

    i

--- src/audio/install_audio_f.py
+++ src/audio/install_audio_f.py
@@ -1,9 +1,19 @@
 from variables import *
 from functions import *
+from config_f import *
 import subprocess
 
 def install_audio():
 
     try: subprocess.run(["pip", "install", "pygame"], check=True)
     except Exception as e: input(e)
+    ico.add("audio-end")
     success("Audio for Parmanode has been installed")
+
+def uninstall_audio():
+
+    try: subprocess.run(["pip", "uninstall", "pgame"], check=True)
+    except Exception as e: input(e)
+    ico.remove("audio-")
+    success("Audio for Parmanode has been uninstalled")
+
