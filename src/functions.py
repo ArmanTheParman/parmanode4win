d8448e3dc9ba9395e7ccc348d200372888babce2
Parman <armantheparman@protonmail.com>
2024-08-17 15:07:54 +1000

    a

--- src/functions.py
+++ src/functions.py
@@ -825,11 +825,9 @@ def tidy_up_before_starting():
             from tor.install_tor_f import install_tor
             install_tor()
     
-    result = subprocess.run(["wsl", "--list", "--quiet"], check=True, capture_output=True, text=True).stdout.strip()
-    normalized_result = result.lower().replace('\r', '').replace('\n', '').strip()
-    print(f"{repr(result)}")
-    print(f"{repr(normalized_result)}")
-    if b"docker" in normalized_result:
-        input("it's true")
-    else:
-        input("not true")
+    result = subprocess.run(["wsl", "--list", "--quiet"], check=True, capture_output=True, text=True).stdout.strip().splitlines()
+    for i in result:
+        if "docker" in result:
+            input("it's true")
+        else:
+            input("not true")
