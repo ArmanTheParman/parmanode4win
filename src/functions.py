6fedb7e6fce6b4bc0842557c5cf21230555ed540
Parman <armantheparman@protonmail.com>
2024-08-17 14:45:31 +1000

    d

--- src/functions.py
+++ src/functions.py
@@ -826,6 +826,7 @@ def tidy_up_before_starting():
             install_tor()
     
     result = subprocess.run(["powershell", "wsl --list --quiet"], check=True, capture_output=True, text=True).stdout.strip()
+    print(result)
     if "docker-desktop" in result:
         input("it's true")
     else:
