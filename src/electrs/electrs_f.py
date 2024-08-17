8f9b40ec94f0ca10a75496dc0591aba4f0edb16b
Parman <armantheparman@protonmail.com>
2024-08-17 19:16:41 +1000

    a

--- src/electrs/electrs_f.py
+++ src/electrs/electrs_f.py
@@ -1,2 +1,17 @@
+import subprocess, os
+from functions import *
+from variables import *
+from config_f import *
+from parmanode.sned_sats_f import *
+
 def install_electrs():
+
+    sned_sats()
+    
+    #Build from dockerfile
+    p4w_electrs = p4w / "src" / "electrs"
+    subprocess.run(f"docker build -t electrs {p4w_electrs}/ ", shell=True)
+    
+    
+def uninstall_electrs():
     pass
