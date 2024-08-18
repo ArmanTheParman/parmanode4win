fcd81a4b2004f10cc702eb83fc616b0e8ee6c47f
Parman <armantheparman@protonmail.com>
2024-08-18 15:33:33 +1000

    d

--- src/electrs/make_electrs_config_f.py
+++ src/electrs/make_electrs_config_f.py
@@ -1,15 +1,15 @@
-import subprocess, os
 from functions import *
 from variables import *
 from config_f import *
 
 def make_electrs_config(db_dir=None):
-
+    input("m1")
     dot_electrs = HOME / ".electrs"
     dot_electrs.mkdir(exist_ok=True)
 
     electrs_config_file = dot_electrs / "config.toml"
 
+    input("m2")
     try:
         rpcuser = bco.grep("rpcuser=", returnline=True).split('=')[1].strip()
         rpcpassword = bco.grep("rpcpassword=", returnline=True).split('=')[1].strip()
@@ -17,6 +17,7 @@ def make_electrs_config(db_dir=None):
         input(e)
         return False
      
+    input("m3")
     config_text=f"""    
 daemon_rpc_addr = \"host.docker.internal:8332\"
 daemon_p2p_addr = \"host.docker.internal:8333\"
@@ -27,6 +28,8 @@ log_filters = \"INFO\" # Options are ERROR, WARN, INFO, DEBUG, TRACE
                        # Changing this will affect parmanode menu output negatively
 auth = \"{rpcuser}:{rpcpassword}\""""
     
+    input("m4")
     with open(electrs_config_file, 'w') as f:
         f.write(config_text)
  
+    input("m5")
