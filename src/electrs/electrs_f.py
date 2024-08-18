3baf0d841cce1eb96534682bffd5733116e05fcc
Parman <armantheparman@protonmail.com>
2024-08-18 16:10:59 +1000

    a

--- src/electrs/electrs_f.py
+++ src/electrs/electrs_f.py
@@ -211,7 +211,8 @@ def docker_run_electrs(db_dir=None):
     dot_electrs = str(HOME / ".electrs")
 
 #    command = ["docker", "run", "-d", "--name", "electrs", "-p", "50005:50005", "--restart", "unless-stopped", "-p", "50006:50006", "-p", "9060:9060", "-v", f"{db_dir}:/electrs_db", "-v", f"{dot_electrs}:/home/parman/.electrs", "electrs"]
-    command = ["docker", "run", "-d", "--name", "electrs", "-p", "50005:50005", "--restart", "unless-stopped", "-p", "50006:50006", "-p", "9060:9060", "electrs"]
+#    command = ["docker", "run", "-d", "--name", "electrs", "-p", "50005:50005", "--restart", "unless-stopped", "-p", "50006:50006", "-p", "9060:9060", "electrs"]
+    command = ["docker", "run", "-d", "--name", "electrs", "-p", "50005:50005", "electrs"]
     
     try: subprocess.run(f"{command}", check=True)
     except Exception as e:
