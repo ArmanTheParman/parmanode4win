66890818558e3e6583626a090aae9e7e99745dda
Parman <armantheparman@protonmail.com>
2024-08-21 09:28:49 +1000

    add menu

--- src/menus/menu_add_f.py
+++ src/menus/menu_add_f.py
@@ -5,6 +5,7 @@ from electrum.install_electrum_f import *
 from tor.install_tor_f import *
 from wsl.wsl_f import *
 from docker.docker_f import *
+from electrs.electrs_f import *
 from config_f import *
 
 def menu_add():
@@ -47,14 +48,21 @@ def menu_add():
             wslmenu = True
             available.append(add_wsl)
         else: 
-            tormenu = False
+            wslmenu = False
 
         if not ico.grep("docker-"):
             add_docker= f"#                  {green} (d){orange}            Docker Desktop {grey}(requires WSL){orange}                       #"
             dockermenu = True
             available.append(add_docker)
         else: 
-            tormenu = False
+            dockermenu = False
+
+        if not ico.grep("electrs-"):
+            add_electrs= f"#                  {green} (ers){orange}            Electrs {grey}(requires Docker){orange}                         #"
+            electrsmenu = True
+            available.append(add_electrs)
+        else: 
+            electrsmenu = False
 
         set_terminal(h=38)
         print(f"""
@@ -107,6 +115,10 @@ def menu_add():
         elif choice.lower() == "d":
             if dockermenu == False : continue
             if not install_docker(): return False
+            return True
+        elif choice.lower() == "ers":
+            if electrsmenu == False : continue
+            if not install_electrs(): return False
             return True
         else:
             invalid()
