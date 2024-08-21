48a16d71c186bb74982ee11ab9453ea8aa233f33
Parman <armantheparman@protonmail.com>
2024-08-21 11:58:06 +1000

    a

--- src/electrs/menu_electrs_f.py
+++ src/electrs/menu_electrs_f.py
@@ -57,7 +57,6 @@ def menu_electrs():
        elif choice.lower() == "stop":
            set_terminal()
            stop_electrs()
-           input("after stop")
            continue 
        elif choice.lower() == "ec":
            file = HOME / ".electrs" / "config.toml"
