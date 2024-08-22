1786627a91e13e8b3f32754387654a5b8348f5da
Parman <armantheparman@protonmail.com>
2024-08-22 10:25:13 +1000

    a

--- src/electrs/menu_electrs_f.py
+++ src/electrs/menu_electrs_f.py
@@ -94,8 +94,9 @@ def menu_electrs():
 
 def _iselectrsrunning():
     try: 
-        output = subprocess.run(["docker", "exec", "electrs", "ps"], capture_output=True, check=True, text=True).stdout.splitlines()
-        for i in output:
+        output1 = subprocess.run(["docker", "exec", "electrs", "ps"], capture_output=True, check=True, text=True, stout=subprocess.PIPE)
+        output2 = subprocess.run(["grep", "-q", "electrs"], stdin=output1).stdout. splitlines()
+        for i in output2:
             if "electrs" in i:
                 return True
         return False
